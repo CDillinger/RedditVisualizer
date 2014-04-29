4b2a464198372c04101b9ed5db7cad13d0e23103
Collin Dillinger <collin.dillinger@gmail.com>
2014-04-29 12:42:38 -0400

    clean up StringConvert

--- Source/RedditVisualizer/Helpers/StringConvert.cs
+++ Source/RedditVisualizer/Helpers/StringConvert.cs
@@ -8,35 +8,35 @@
 			{
 				string part1 = xmlString.Substring(0, xmlString.IndexOf("&quot;"));
 				string part2 = xmlString.Substring(xmlString.IndexOf("&quot;") + 6);
-				xmlString = part1 + '"'.ToString() + part2;
+				xmlString = part1 + '"' + part2;
 			}
 
 			while (xmlString.Contains("&amp;"))
 			{
 				string part1 = xmlString.Substring(0, xmlString.IndexOf("&amp;"));
 				string part2 = xmlString.Substring(xmlString.IndexOf("&amp;") + 5);
-				xmlString = part1 + '&'.ToString() + part2;
+				xmlString = part1 + '&' + part2;
 			}
 
 			while (xmlString.Contains("&apos;"))
 			{
 				string part1 = xmlString.Substring(0, xmlString.IndexOf("&apos;"));
 				string part2 = xmlString.Substring(xmlString.IndexOf("&apos;") + 6);
-				xmlString = part1 + '&'.ToString() + part2;
+				xmlString = part1 + "'" + part2;
 			}
 
 			while (xmlString.Contains("&lt;"))
 			{
 				string part1 = xmlString.Substring(0, xmlString.IndexOf("&lt;"));
 				string part2 = xmlString.Substring(xmlString.IndexOf("&lt;") + 4);
-				xmlString = part1 + '<'.ToString() + part2;
+				xmlString = part1 + '<' + part2;
 			}
 
 			while (xmlString.Contains("&gt;"))
 			{
 				string part1 = xmlString.Substring(0, xmlString.IndexOf("&gt;"));
 				string part2 = xmlString.Substring(xmlString.IndexOf("&gt;") + 4);
-				xmlString = part1 + '>'.ToString() + part2;
+				xmlString = part1 + '>' + part2;
 			}
 
 			return xmlString;
